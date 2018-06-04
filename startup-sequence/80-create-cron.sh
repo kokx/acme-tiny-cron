b87ac7a2098b1e3b889cc784977f650b74d46fb3
Pieter Kokx <pieter@kokx.nl>
2018-06-04 16:05:06 +0200

    Set the time at which the cron is executed

--- startup-sequence/80-create-cron.sh
+++ startup-sequence/80-create-cron.sh
@@ -1,5 +1,7 @@
 # TODO: later make this monthly, for now it is not for testing
 
-echo '* * * * * /exec.sh' > /tmp/cron
+# 1:15 A.M. on the 5th of every month
+
+echo '15 1 5 * * /exec.sh' > /tmp/cron
 
 crontab /tmp/cron
